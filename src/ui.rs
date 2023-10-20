d82b15de658ca11d069a9efad55c0cd6828c311a
Amos Elmaliah <amosel@gmail.com>
2023-10-20 14:47:08 -0400

    ui update to render the list

--- src/ui.rs
+++ src/ui.rs
@@ -1,36 +1,64 @@
 use tui::{
     backend::Backend,
-    layout::Alignment,
-    style::{Color, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::Line,
+    widgets::*,
     Frame,
 };
-
 use crate::app::App;
 
 /// Renders the user interface widgets.
 pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
+        .split(frame.size());
+
     // This is where you add new widgets.
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/examples
     frame.render_widget(
         Paragraph::new(format!(
-            "This is a tui template.\n\
+            "\
                 Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
+                Press up and down to navigate and space bad to toggle selection\n\
                 Counter: {}",
             app.counter
         ))
         .block(
             Block::default()
-                .title("Template")
+                .title("node_modules Cleaner")
                 .title_alignment(Alignment::Center)
                 .borders(Borders::ALL)
                 .border_type(BorderType::Rounded),
         )
         .style(Style::default().fg(Color::Cyan).bg(Color::Black))
         .alignment(Alignment::Center),
-        frame.size(),
-    )
+        layout[0],
+    );
+
+    let items: Vec<ListItem> = app
+        .list
+        .items
+        .iter()
+        .map(|i| {
+            let lines = vec![Line::from(i.title().clone())];
+            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    // Create a List from all list items and highlight the currently selected one
+    let items = List::new(items)
+        .block(Block::default().borders(Borders::ALL).title("Directories"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+
+    // We can now render the item list
+    frame.render_stateful_widget(items, layout[1], &mut app.list.state);
 }
